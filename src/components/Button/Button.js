b7d20462eac1f31fa918e3ad77e0aec7d9e285b5
Jonathan Brant <jbrant1995@gmail.com>
2021-07-30 12:38:20 -0300

    callback function

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Refresh } from './styles.js'
 
 const Button = () => {
 
-    function refreshPage(){ 
-        window.location.reload(); 
-    }
+    const refreshPage = useCallback(
+        () => {
+            window.location.reload();
+        }, [],
+    );
+
     return (
         <Refresh type="button" onClick={ refreshPage }> <img src="refresh.png" alt="Recarregar página"></img></Refresh> 
 
